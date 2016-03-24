ae4875a173429f36030e95b6e2b181fa0f82321f
Boislard, Pierre <Pierre.Boislard@edelman.com>
2016-03-24 12:41:53 -0400

    stuff

---
+++ public/local/app/Http/Controllers/Backend/UsersController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Cms\Http\Controllers\Backend;
+
+use Cms\User;
+use Illuminate\Http\Request;
+use Cms\Http\Requests;
+
+
+class UsersController extends Controller
+{
+
+    protected $users;
+
+    public function __construct(User $users)
+    {
+        $this->users = $users;
+
+        parent::__construct();
+    }
+
+    public function index(){
+        $users = $this->users->paginate(10);
+
+        return view('backend.users.index', compact('users'));
+    }
+
+    public function create(User $user){
+
+        return view('backend.users.form', compact('user'));
+    }
+
+    public function store(Requests\StoreUserRequest $request){
+        $this->users->create($request->only('name', 'email', 'password'));
+
+        return redirect(route('backend.users.index'))->with('status', 'User has been created.');
+    }
+
+    public function edit($id){
+        $user = $this->users->findOrFail($id);
+
+        return view('backend.users.form', compact('user'));
+    }
+
+    public function update(Requests\UpdateUserRequest $request, $id){
+
+        $user = $this->users->findOrFail($id);
+        $user->fill($request->only('name', 'email', 'password'))->save();
+
+        return redirect(route('backend.users.edit', $user->id))->with('status', 'User has been updated');
+    }
+
+    public function confirm($id){
+
+    }
+
+    public function destroy($id){
+
+    }
+}
--- public/local/public/themes/default/views/layouts/backend.blade.php
+++ public/local/public/themes/default/views/layouts/backend.blade.php
@@ -12,9 +12,7 @@
 <nav class="navbar navbar-static-top navbar-inverse">
     <div class="navbar-header"><a href="/" class="navbar-brand">Laravel CMS</a></div>
     <ul class="nav navbar-nav">
-        <li><a href="#">Item 1</a></li>
-        <li><a href="#">Item 1</a></li>
-        <li><a href="#">Item 1</a></li>
+        <li><a href="{{ route('backend.users.index') }}">Users</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
         <li><span class="navbar-text">Hello, {{ $admin->name }}</span></li>
@@ -25,6 +23,18 @@
     <div class="row">
         <div class="col-md-12">
             <h3>@yield('title')</h3>
+
+            @if($errors->any())
+                <div class="alert alert-danger">
+                    <strong> We found some errors!</strong>
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
 
             @yield('content')
         </div>
